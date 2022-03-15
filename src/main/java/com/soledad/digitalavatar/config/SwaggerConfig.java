c140b284d396e0cf61e7da686f9fccfe1ee0e21c
Soledad-X <5653801+soledad-x@user.noreply.gitee.com>
2022-03-16 00:21:40 +0800

    项目集成Swagger

---
+++ src/main/java/com/soledad/digitalavatar/config/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package com.soledad.digitalavatar.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@EnableOpenApi
+public class SwaggerConfig {
+    @Bean //配置docket以配置Swagger具体参数
+    public Docket docket() {
+        return new Docket(DocumentationType.OAS_30);
+    }
+}
