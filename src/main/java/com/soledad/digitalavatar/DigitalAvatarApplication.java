45f23d0da4be355688d4dfa773fa3e2a8d1c44d6
Soledad-X <5653801+soledad-x@user.noreply.gitee.com>
2022-03-15 22:47:44 +0800

    新建SpringBoot-web项目

---
+++ src/main/java/com/soledad/digitalavatar/DigitalAvatarApplication.java
@@ -0,0 +1,13 @@
+package com.soledad.digitalavatar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalAvatarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalAvatarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/soledad/digitalavatar/DigitalAvatarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soledad.digitalavatar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalAvatarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
